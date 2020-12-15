40a1d38c961b3b035726fa1c23bf5aa6f579961a
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-12-15 00:14:30 +0000

    Added Documentation

--- pyxcel/sheet_manager.py
+++ pyxcel/sheet_manager.py
@@ -1,13 +1,22 @@
-import xlwings as xw
-from xlwings.main import Range
-from typing import Any
 import datetime
 import re
+from typing import Any
+
+import xlwings as xw
+from xlwings.main import Range
 
 
 class SheetManager:
 
     def __init__(self, path: str) -> None:
+
+        """Class for Managing Excel Sheets.
+
+        Parameters
+        ----------
+        path: str
+            Path to Excel Workbook.
+        """
 
         self.path = path
         self._verify_path(path)
@@ -16,24 +25,77 @@ class SheetManager:
         self.backup_path = self._store_backup()
 
     def __del__(self) -> None:
+
+        """Ensures that Workbook is saved and closed."""
+
         self.wb.save()
         self.wb.close()
 
     def get_cell(self, sheet: str, cell: str) -> Range:
+
+        """Returns contents of given cell/s.
+
+        Parameters
+        ----------
+        sheet: str
+            Sheet to point to.
+        cell: str
+            Cell to return.
+
+        Returns
+        -------
+        Range
+            Excel cell/s.
+        """
+
         return self.wb.sheets[sheet].range(cell)
 
     def change_cell(self, sheet: str, cell: str, value: Any):
+
+        """Change value in given cell/s.
+
+        Parameters
+        ----------
+        sheet: str
+            Sheet to point to.
+        cell: str
+            Cell to change.
+        value: Any
+            Value/s to prescribe to specified cell/s.
+        """
+
         self.get_cell(sheet, cell).value = value
 
     def check_cell(self, sheet: str, cell: str) -> Any:
+
+        """Gets value/s of specified cell/s.
+
+        Parameters
+        ----------
+        sheet: str
+            Sheet to point to.
+        cell: str
+            Cell to check.
+
+        Returns
+        -------
+        Any
+            Value/s of specified cell/s.
+        """
+
         return self.get_cell(sheet, cell).value
 
     def restore(self) -> None:
+
+        """Restore Workbook from Backup."""
+
         self.wb.close()
         self.wb = xw.Book(self.backup_path)
         self.wb.save(self.path)
 
     def _store_backup(self) -> str:
+
+        """Store Backup of Workbook."""
 
         self.wb.save()
 
@@ -48,5 +110,14 @@ class SheetManager:
 
     @staticmethod
     def _verify_path(path: str) -> None:
+
+        """Verifies file is .xlsx.
+
+        Parameters
+        ----------
+        path: str
+            Path to verify.
+        """
+
         if not path.endswith('.xlsx'):
             raise ValueError('File must have .xlsx extension.')
